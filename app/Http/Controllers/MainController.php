ea0c51c8d0eb667564a7581cef1453eada668dc2
DenisMozhaev <d.mozhaev@digital-sector.ru>
2023-08-12 18:53:07 +0300

    am_3 | filter

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\DictService;
+use Illuminate\Database\Eloquent\Collection;
+
 class MainController extends Controller
 {
+    private DictService $service;
 
+    public function __construct(DictService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getBrands(): Collection
+    {
+        return $this->service->getBrandList();
+    }
 }
