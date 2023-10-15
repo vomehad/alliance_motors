150ce43a920c4af3c256a56aa73b6660a468731d
DenisMozhaev <d.mozhaev@digital-sector.ru>
2023-10-15 23:06:34 +0300

    am_3 | seed

--- database/seeders/PersonSeeder.php
+++ database/seeders/PersonSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Person;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use \Faker\Provider\ru_RU\Person as FakePerson;
 
 class PersonSeeder extends Seeder
 {
@@ -20,19 +21,21 @@ class PersonSeeder extends Seeder
             $name = Arr::get($person, 'name');
             $surname = Arr::get($person, 'surname');
             $job = Arr::get($person, 'job');
+            $department = Arr::get($person, 'department');
 
             if (!Person::query()->where(['name' => $name, 'surname' => $surname])->exists()) {
-                $this->addPerson($name, $surname, $job);
+                $this->addPerson($name, $surname, $job, $department);
             }
         }
     }
 
-    private function addPerson(string $name, string $surname, string $job): void
+    private function addPerson(string $name, string $surname, string $job, string $department): void
     {
         $user = new Person();
         $user->name = $name;
         $user->surname = $surname;
         $user->job = $job;
+        $user->department = $department;
 
         if ($user->save()) {
             (new ConsoleOutput())->writeln("Сотрудник $user->name $user->surname");
@@ -44,63 +47,75 @@ class PersonSeeder extends Seeder
         return [
             [
                 "name" => "Нибиуллин",
-                "surname" => "Константин",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
                 "name" => "Приреченский",
-                "surname" => "Владислав",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
                 "name" => "Михаливченков",
-                "surname" => "Константин",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
                 "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
+            ],
+            [
+                "name" => "Рандомный",
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
                 "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
                 "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "surname" => FakePerson::firstNameMale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::SALES_DEPARTMENT,
             ],
             [
-                "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "name" => "Волобуева",
+                "surname" => FakePerson::firstNameFemale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::CREDIT_DEPARTMENT,
             ],
             [
-                "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "name" => "Нибиуллина",
+                "surname" => FakePerson::firstNameFemale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::CREDIT_DEPARTMENT,
             ],
             [
-                "name" => "Нибиуллин",
-                "surname" => "Константин",
-                "job" => "Менеджер по продажам"
+                "name" => "Приреченская",
+                "surname" => FakePerson::firstNameFemale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::CREDIT_DEPARTMENT,
             ],
             [
-                "name" => "Приреченский",
-                "surname" => "Владислав",
-                "job" => "Менеджер по продажам"
+                "name" => "Михаливченкова",
+                "surname" => FakePerson::firstNameFemale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::CREDIT_DEPARTMENT,
             ],
             [
-                "name" => "Михаливченков",
-                "surname" => "Константин",
-                "job" => "Менеджер по продажам"
-            ],
-            [
-                "name" => "Волобуев",
-                "surname" => "Кирилл",
-                "job" => "Менеджер по продажам"
+                "name" => "Волобуева",
+                "surname" => FakePerson::firstNameFemale(),
+                "job" => "Менеджер по продажам",
+                "department" => Person::CREDIT_DEPARTMENT,
             ],
         ];
     }
