58e11866ece157f0c1ba09322b085d81285e1ed8
d.mozhaev <d.mozhaev@digital-sector.ru>
2023-11-27 13:07:40 +0300

    am_3 | email заявки

--- app/Mail/SampleMail.php
+++ app/Mail/SampleMail.php
@@ -3,10 +3,7 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class SampleMail extends Mailable
@@ -23,10 +20,9 @@ class SampleMail extends Mailable
     public function build(): SampleMail
     {
         return $this->subject($this->content['subject'])
-            ->view('emails.sample', [
+            ->view('emails.application', [
                 'content' => $this->content,
-            ])
-            ;
+            ]);
 
     }
 
